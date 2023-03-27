2def8d956fed944a63d94de49a17913c81fadf47
Martins Daniels <danielsmrtns@gmail.com>
2023-03-27 01:37:00 -0400

    added default stringlength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,33 +5,33 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+   /**
+    * Register any application services.
+    *
+    * @return void
+    */
+   public function register()
+   {
+      //
+   }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-        View::composer(['admin.*', 'auth.*'], function($view){
-            $authUser = auth()->user();
-            return $view->with(compact('authUser'));
-        });
-
-    }
+   /**
+    * Bootstrap any application services.
+    *
+    * @return void
+    */
+   public function boot()
+   {
+      Schema::defaultStringLength(191);
+      View::composer(['admin.*', 'auth.*'], function ($view) {
+         $authUser = auth()->user();
+         return $view->with(compact('authUser'));
+      });
+   }
 }
