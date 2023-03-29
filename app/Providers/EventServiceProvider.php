c4ab113f2aa71c773e95528e3c53074f64cc7b9a
Martins Daniels <danielsmrtns@gmail.com>
2023-03-29 02:40:14 -0400

    Email Notification implemented

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,40 +5,42 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use App\Listeners\GenerateSavingsAzaForNewUser;
+use App\Listeners\SendWelcomeMail;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-            GenerateSavingsAzaForNewUser::class,
-        ],
-    ];
+   /**
+    * The event to listener mappings for the application.
+    *
+    * @var array<class-string, array<int, class-string>>
+    */
+   protected $listen = [
+      Registered::class => [
+         SendWelcomeMail::class,
+         SendEmailVerificationNotification::class,
+         GenerateSavingsAzaForNewUser::class,
+      ],
+   ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+   /**
+    * Register any events for your application.
+    *
+    * @return void
+    */
+   public function boot()
+   {
+      //
+   }
 
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
-     */
-    public function shouldDiscoverEvents()
-    {
-        return false;
-    }
+   /**
+    * Determine if events and listeners should be automatically discovered.
+    *
+    * @return bool
+    */
+   public function shouldDiscoverEvents()
+   {
+      return false;
+   }
 }
---
+++ resources/views/mail/welcome-new-user.blade.php
@@ -0,0 +1,30 @@
+<x-mail::message>
+    Dear {{ ucfirst($username) }},
+
+    We would like to extend a warm welcome to you as a new member of the Bluebird Global Express family! We are thrilled
+    to have you as our customer and look forward to serving you with our comprehensive range of banking services.
+
+    At Bluebird Global Express, we strive to provide you with the best-in-class banking experience, and we believe that
+    you will find our services to be convenient, reliable, and secure. Whether you need assistance with everyday banking
+    transactions, financing for your business, or investment advice, our team of experts is always ready to assist you.
+
+    We also offer a range of digital banking options, including mobile banking, online banking, and telephone banking,
+    which allow you to manage your accounts from the comfort of your home or office. With our digital banking services,
+    you can transfer funds, pay bills, check your account balance, and more, all with just a few clicks.
+
+    Thank you for choosing Bluebird Global Express as your banking partner. We are committed to providing you with
+    exceptional service and look forward to helping you achieve your financial goals.
+
+    Best regards,
+
+    Landon,
+
+    Bluebird Global Express Limited
+
+    {{-- <x-mail::button :url="''">
+        Button Text
+    </x-mail::button> --}}
+
+    {{-- Thanks,
+    {{ config('app.name') }} --}}
+</x-mail::message>
