24c38e1eb8315c77319bd36ca71d3bf769e9b50e
Martins Daniels <danielsmrtns@gmail.com>
2023-04-04 10:29:33 -0400

    removed the map from the contact page

--- resources/views/website/www-contact.blade.php
+++ resources/views/website/www-contact.blade.php
@@ -81,11 +81,11 @@
             </form>
         </div>
     </div>
-    <div class="map">
+    {{-- <div class="map">
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10784.188505644011!2d19.053119335158936!3d47.48899529453826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1543907528304"
             style="border:0" allowfullscreen></iframe>
-    </div>
+    </div> --}}
     </div>
 </section>
 
